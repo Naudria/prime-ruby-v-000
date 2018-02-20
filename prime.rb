17ff2f426b3cbdc4bb923ebee118684b2cbb4721
Rima Jean <rima.jean@gmail.com>
2018-02-20 22:32:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
-def prime?(number)
-  number >= 1
-  primes = (1..number).select do {|n| number % n == 0}
-    primes.length < 3
-end
+def prime?(num)
+  return false if num <= 1
+  factors = (1..num).select { |n| num % n == 0}
+  factors.length < 3
+ end
